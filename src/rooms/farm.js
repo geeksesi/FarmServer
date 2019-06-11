a038d7217996138dbf8cf32090441cbad004e3b7
mohammad javad ghasemy <geeksesi@gmail.com>
2019-06-12 03:47:40 +0430

    make auth on user

--- src/rooms/farm.js
+++ src/rooms/farm.js
@@ -1,40 +1,70 @@
 const colyseus = require('colyseus');
 // const { encrypt, decrypt } = require('./../controller/security');
-const { Build } = require('./../db/module');
-const { add_build, update_build, delete_build } = require('./../db/set');
+const { Build, User } = require('./../db/module');
+const { add_build, update_build, delete_build, add_user } = require('./../db/set');
 // const passwordHash = require('password-hash');
 
 module.exports = class extends colyseus.Room {
 
     onInit(options) {
+        this.autoDispose = false;
         this.authed_client = {};
         console.log("init");
     }
-    
+
     onAuth(options) {
         console.log("auth");
-        return true;
+        let user_id = options.user_id;
+        let promise = new Promise((resolve, reject) => {
+            User.find({ _id: user_id }, (err, user) => {
+                if (err !== null || user == null || user.length === 0) {
+                    add_user((err, user) => {
+                        if (err) {
+                            reject(false);
+                        } else {
+                            resolve(user);
+                        }
+                    });
+
+                } else {
+                    resolve(user);
+                }
+            });
+        })
+        return promise;
     }
 
 
 
     onJoin(client, options, auth) {
+        if (auth === false) {
+            return false;
+        }
+        this.authed_client[client.id] = {
+            user_id: auth._id,
+            xp: auth.xp,
+            soft_currency: auth.soft_currency,
+            hard_currency: auth.hard_currency,
+            ws: client
+        }
         Build.find({}, (err, build) => {
-            if(err)
-            {
+            if (err) {
                 this.send(client, {
                     ok: false,
                     message_type: "init",
                     message: "can't access to database",
-                    data: err
+                    data: err,
+                    user:auth
                 });
 
                 return false;
             }
             this.send(client, {
                 ok: true,
+                message: "here your init data",
                 message_type: "init",
-                data: build
+                data: build,
+                user : auth
             });
             // console.log(build);
         })
@@ -65,7 +95,6 @@ module.exports = class extends colyseus.Room {
                         message: `build maded by id : ${res.body._id}`,
                         data: {
                             id: res.body._id,
-                            message_type: "on_add",
                             type: res.body.type,
                             location: res.body.location
                         }
